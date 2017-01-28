b0b0a0a0ee3ad0bbb661b4277099d1b8ace8c564
luispimenta <luis.pimenta@jumia.com>
2017-01-28 19:54:16 +0000

    3. skeleton blocks

---
+++ tests/weekDayTests.js
@@ -0,0 +1,13 @@
+/* global describe, it, require, global */
+/**
+ * Created by luispimenta on 28/01/17.
+ */
+
+var assert = require('chai').assert;
+var weekDay = require('../app/weekday');
+
+
+describe("Weekday - getWeekDay", function () {
+    "use strict";
+    it("Should return correct day for monday");
+});
