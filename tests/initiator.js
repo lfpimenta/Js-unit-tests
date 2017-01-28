81570686c881b87307f1fc622f53cb06caec3192
luispimenta <luis.pimenta@jumia.com>
2017-01-28 20:57:05 +0000

    Initiator add requires necessary

---
+++ tests/initiator.js
@@ -0,0 +1,14 @@
+/* global describe, it, require, global */
+/**
+ * Created by luispimenta on 28/01/17.
+ */
+var assert = require('chai').assert;
+var initiatorPath = '../app/initiator.js';
+var initiator = require(initiatorPath);
+
+
+
+
+
+
+
