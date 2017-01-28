6ed0a8409da7cea617e80a957017bc7c55b2a862
luispimenta <luis.pimenta@jumia.com>
2017-01-28 21:43:03 +0000

    Initiator 4.3 Initialize refactor

--- tests/initiator.js
+++ tests/initiator.js
@@ -3,24 +3,25 @@
  * Created by luispimenta on 28/01/17.
  */
 var assert = require('chai').assert;
-var initiator = require('../app/initiator');
+var initiatorPath = '../app/initiator';
 
 describe("Initiator - formatMoney", function () {
     "use strict";
     it("Number must have formatMoney prototype function", function () {
+        setStoreAndInitialize();
         var number = 12;
         assert.equal(typeof number.formatMoney, "function", "Property formatMoney not found");
     });
 
     it("Number format should be correct", function () {
-        setStore();
+        setStoreAndInitialize();
         var number = 1200;
         assert.equal(number.formatMoney(), "€ 1.200,00");
     });
 
 });
 
-function setStore(decimal, symbol, sposition, tposition, dseparator) {
+function setStoreAndInitialize(decimal, symbol, sposition, tposition, dseparator) {
     "use strict";
     global.store = {
         currency: {
@@ -31,4 +32,12 @@ function setStore(decimal, symbol, sposition, tposition, dseparator) {
             decimalSeparator: dseparator || ','
         }
     };
+    reloadInitiator();
+}
+
+function reloadInitiator() {
+    "use strict";
+    delete Number.prototype.formatMoney;
+    delete require.cache[require.resolve(initiatorPath)];
+    require(initiatorPath);
 }
