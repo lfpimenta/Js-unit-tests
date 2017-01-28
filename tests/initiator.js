396f112a947824d30a156514e24dbf318ab8539d
luispimenta <luis.pimenta@jumia.com>
2017-01-28 21:13:03 +0000

    Initiator 4.1 Number has prototype formatMoney

--- tests/initiator.js
+++ tests/initiator.js
@@ -7,5 +7,8 @@ var initiator = require('../app/initiator');
 
 describe("Initiator - formatMoney", function () {
     "use strict";
-    it("Number must have formatMoney prototype function");
+    it("Number must have formatMoney prototype function", function () {
+        var number = 12;
+        assert.equal(typeof number.formatMoney, "function", "Property formatMoney not found");
+    });
 });
