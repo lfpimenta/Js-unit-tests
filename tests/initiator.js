03e6b7b32082c8af5a20e8517d6c27b9ca1be5ef
luispimenta <luis.pimenta@jumia.com>
2017-01-28 21:28:18 +0000

    Initiator 4.2 Number should be correctly format

--- tests/initiator.js
+++ tests/initiator.js
@@ -11,4 +11,24 @@ describe("Initiator - formatMoney", function () {
         var number = 12;
         assert.equal(typeof number.formatMoney, "function", "Property formatMoney not found");
     });
+
+    it("Number format should be correct", function () {
+        setStore();
+        var number = 1200;
+        assert.equal(number.formatMoney(), "€ 1.200,00");
+    });
+
 });
+
+function setStore(decimal, symbol, sposition, tposition, dseparator) {
+    "use strict";
+    global.store = {
+        currency: {
+            decimalDigits: (decimal === undefined) ? 2 : decimal,
+            symbol: symbol || '€',
+            symbolPosition: sposition || 'prefix',
+            thousandsSeparator: tposition || '.',
+            decimalSeparator: dseparator || ','
+        }
+    };
+}
