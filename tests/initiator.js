e88944be437422ab3629b35f3f814a8398e6dd09
luispimenta <luis.pimenta@jumia.com>
2017-01-28 21:52:17 +0000

    Initiator 4.4 data provider

--- tests/initiator.js
+++ tests/initiator.js
@@ -19,6 +19,16 @@ describe("Initiator - formatMoney", function () {
         assert.equal(number.formatMoney(), "€ 1.200,00");
     });
 
+    var doTest = function(params, index) {
+        it("formatMoney Scenario " + index, function() {
+            setStoreAndInitialize.apply(null, params.store);
+            var number = params.input;
+            assert.equal(params.expected, number.formatMoney());
+        });
+    };
+
+    getDataProvider().forEach(doTest);
+
 });
 
 function setStoreAndInitialize(decimal, symbol, sposition, tposition, dseparator) {
@@ -40,4 +50,52 @@ function reloadInitiator() {
     delete Number.prototype.formatMoney;
     delete require.cache[require.resolve(initiatorPath)];
     require(initiatorPath);
+}
+
+/**
+ * Data Provider
+ * @returns {Array}
+ */
+function getDataProvider ()
+{
+    "use strict";
+    var dataProvider = [];
+    dataProvider.push(
+        {
+            input: 1200,
+            store: [],
+            expected: '€ 1.200,00'
+        },
+        {
+            input: 1200.45,
+            store: [],
+            expected: '€ 1.200,45'
+        },
+        {
+            input: 1200.45,
+            store: [2, '$', 'suffix', ',', '.'],
+            expected: '1,200.45 $'
+        },
+        {
+            input: 1200.45,
+            store: [1, '$', 'suffix', ',', '.'],
+            expected: '1,200.5 $'
+        },
+        {
+            input: 1200.45,
+            store: [0, '$', 'suffix', ',', '.'],
+            expected: '1,200 $'
+        },
+        {
+            input: 12.45,
+            store: [0, '$', 'suffix', ',', '.'],
+            expected: '12 $'
+        },
+        {
+            input: NaN,
+            store: [0, '$', 'suffix', ',', '.'],
+            expected: '0 $'
+        }
+    );
+    return dataProvider;
 }
